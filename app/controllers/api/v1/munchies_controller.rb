eabaa059fe421e3e44be45091921c24153e1d74e
Matt Deming <vballmatt44@gmail.com>
2022-08-08 11:49:20 -0600

    Receiving 200 status and getting response, I think

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::MunchiesController < ApplicationController
 
   def index
-    
+
+    yelp = YelpFacade.food_loc_search(params[:location], params[:term])
+
+    render json: MunchiesSerializer.new(yelp)
   end
 end
