7866492ecc20cbc1d9c71c10df7d0f7d1fe04822
Matt Deming <vballmatt44@gmail.com>
2022-08-08 13:55:18 -0600

    Fix hidden key

--- app/services/yelp_service.rb
+++ app/services/yelp_service.rb
@@ -9,7 +9,7 @@ class YelpService
     response = conn.get(end_point) do |f|
       f.params['location'] = loc
       f.params['term'] = term
-      f.headers['Authorization'] = 'Bearer 9P-9u2FoznEgxBtM8C0PBm6VLELEJdbI8sxFYXEMVGEfQaH7biHFh54kooaH5UBJWa4SyZ2N3fZ-EzUetNZGnCtuJ9ytGTgIqq417OhaQqSBkcUukznUWL5k_ifxYnYx'
+      f.headers['Authorization'] = ENV['yelp_key']
     end
     JSON.parse(response.body, symbolize_names: true)
   end
